17a1c74eeee70d39d0bc66268814dfd7e33d7658
Fredy Sierra <fredy@backbase.com>
2025-01-02 12:45:57 +0100

    feat: Adding spring boot application

---
+++ src/main/java/com/fysu/testagentactions/TestagentactionsApplication.java
@@ -0,0 +1,13 @@
+package com.fysu.testagentactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestagentactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestagentactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fysu/testagentactions/TestagentactionsApplicationTests.java
@@ -0,0 +1,22 @@
+package com.fysu.testagentactions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestagentactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void testHello() {
+		HelloController helloController = new HelloController();
+		String result = helloController.hello();
+		assertEquals("Hello, World!", result);
+	}
+
+}
